fb2f9f24011907f2749e12644386b4f97987b9e0
Stanislav Matas <stanislav.matas@gmail.com>
2021-02-06 20:40:03 +0100

    fix get_absolute_url of Post, add project-wise static and templates folder, fix blog/ url path, add first html and css

--- blog/models.py
+++ blog/models.py
@@ -39,7 +39,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       [self.publish.year,
-                        self.publish.month,
-                        self.publish.day,
-                        self.slug])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
