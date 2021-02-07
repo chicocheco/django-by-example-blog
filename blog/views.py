19396db578f84899638b098020f43377e7586034
Stanislav Matas <stanislav.matas@gmail.com>
2021-02-07 10:11:37 +0100

    add function-based pagination to post_list

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()  # custom Manager
+    object_list = Post.published.all()  # custom Manager
+    paginator = Paginator(object_list, 3)  # three posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer delver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts})  # the Page object
 
 
 def post_detail(request, year, month, day, post):
