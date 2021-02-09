b10df62f74824f019d92fa464b24dc3cb1cbd02c
Stanislav Matas <stanislav.matas@gmail.com>
2021-02-09 20:06:24 +0100

    extend post_detail by comment form logic, add comments counter, comments and form to detail template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+
+from .forms import EmailPostForm, CommentForm
 from .models import Post
-from .forms import EmailPostForm
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -18,9 +20,26 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':  # comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # assign the current post to the comment
+            new_comment.post = post
+            # save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,  # if exists, show success msg
+                   'comment_form': comment_form})
 
 
 def post_share(request, post_id):
