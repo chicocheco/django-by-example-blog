b072dd50d683ffd21c7bebde742f38455cd32041
Stanislav Matas <stanislav.matas@gmail.com>
2021-02-09 12:24:22 +0100

    add share form (form, view, urlpath, templates)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post
-
+from .forms import EmailPostForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -20,3 +21,23 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cd["name"]} recommends you read {post.title}'
+            message = f'Read {post.title} at {post_url}\n\n{cd["name"]}\'s comment: {cd["comment"]}'
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
