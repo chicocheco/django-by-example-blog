93285f9640622493db9443f7aa31c5212579f9b5
Stanislav Matas <stanislav.matas@gmail.com>
2021-02-15 23:15:48 +0100

    add RSS feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PostListView, post_list, post_detail, post_share
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -11,4 +12,5 @@ urlpatterns = [
          post_detail,
          name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
